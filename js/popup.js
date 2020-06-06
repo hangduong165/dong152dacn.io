f1697ddf9bc2932a1810925b248ca5f6953d0683
hangduong165 <63143006+hangduong165@users.noreply.github.com>
2020-06-06 14:26:40 +0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,1 @@
-var div = document.createElement('div');
-div.style = "position: fixed; bottom: 0; right: 0; z-index:9999";
-div.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/xCkcSL3zfJk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-
-document.body.appendChild(div);
+console.log('hello');
